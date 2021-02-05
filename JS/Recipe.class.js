655d51bea84394f95d568c94e201e05a33668e75
Poccardl <loic.poccard@gmail.com>
2021-02-05 12:24:13 +0100

    add static method insertAllRecipe in Recipe class

--- JS/Recipe.class.js
+++ JS/Recipe.class.js
@@ -1,5 +1,4 @@
 export default class Recipe {
-    /*
     constructor(recipes) {
         this.id = recipes.id;
         this.name = recipes.name;
@@ -10,5 +9,54 @@ export default class Recipe {
         this.appliance = recipes.appliance;
         this.ustensils = recipes.ustensils;
     }
-    */
+
+    static insertAllRecipe(data) {
+        /* TODO: add commentaire */
+        const recipes_section = document.querySelector("#recipes_section .row")
+        let recipe_cards = []
+        for (let element in data) {
+            let recipe_card_html = `<div class="col-xl-4 mt-4">
+            <div class="card">
+            <img src="img/recette_proto.png" class="card-img-top" alt="image test">
+            <div class="card-body">
+            <div class="card_header">
+            <h5 class="card-title">${data[element]["name"]}</h5>
+            <span class="time">
+            <i class="far fa-clock"></i>
+            <b>&#160;${data[element]["time"]} min</b>
+            </span>
+            </div>
+            <div class="card_content">
+            <ul class="element">
+            `
+            let ingredients = data[element]["ingredients"]
+            let recipe_ingredients_html = ""
+            for (let i in ingredients) {
+                recipe_ingredients_html += `<li><b>${ingredients[i]["ingredient"]}</b>:`
+                if (ingredients[i]["quantity"]) {
+                    recipe_ingredients_html += ingredients[i]["quantity"]
+                }
+                if (ingredients[i]["unit"]) {
+                    recipe_ingredients_html += ingredients[i]["unit"]
+                }
+                else {
+                    continue
+                }
+                recipe_ingredients_html += `</li>`
+
+            }
+            recipe_card_html += `${recipe_ingredients_html}
+            </ul>
+            <p class="card-text element">${data[element]["description"]}</p>
+            </div>
+            </div>
+            </div>
+            </div>
+            `
+            recipe_cards.push(recipe_card_html)
+        }
+        for (let element in recipe_cards) {
+            recipes_section.insertAdjacentHTML("beforeend", recipe_cards[element]);
+        }
+    }
 }
--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,19 @@
+// imports
+import Recipe from "./Recipe.class.js"
+
+fetchData()
+
+// DOM Elements
+const search_bar = document.getElementById("search_bar")
+
+// Events
+search_bar.addEventListener("input", (e) => {
+    let search_value = e.target.value
+    if (search_value.length >= 3) {
+        //lance la recherche à partir de 3 caractère
+    }
+})
+
 function fetchData() {
     /* TODO: add commentaire */
     fetch("data/recipes.json")
@@ -5,8 +21,11 @@ function fetchData() {
         return resp.json()
     })
     .then(function(data) {
-        console.log("data :", data["recipes"])
+        let recipes_obj_list = []
+        for (let element in data["recipes"]) {
+            let recipe_obj = new Recipe(data["recipes"][element])
+            recipes_obj_list.push(recipe_obj)
+        }
+        Recipe.insertAllRecipe(recipes_obj_list)
     })
 }
-
-fetchData()
